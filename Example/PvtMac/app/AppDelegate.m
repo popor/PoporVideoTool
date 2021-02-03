48fd448accc5ea246003567fd58e7df6588bcf7f
popor <908891024@qq.com>
2021-02-03 10:32:43 +0800

    增加置顶按钮

--- Example/PvtMac/app/AppDelegate.m
+++ Example/PvtMac/app/AppDelegate.m
@@ -7,9 +7,10 @@
 //
 
 #import "AppDelegate.h"
+#import <Masonry/Masonry.h>
 
 @interface AppDelegate ()
-
+@property (nonatomic, strong) NSButton * frontBT;
 
 @end
 
@@ -23,9 +24,38 @@
         [[NSBundle bundleWithPath:macOSInjectionPath] load];
     }
 #endif
-
     
-    [self keepAtFrontAction:NO];
+    [self addFrontBt];
+}
+
+- (void)addFrontBt {
+    NSApplication * app     = [NSApplication sharedApplication];
+    NSWindow * window       = app.windows.firstObject;
+    NSButton * closeButton  = [window standardWindowButton:NSWindowCloseButton];
+    NSView   * titleBarView = closeButton.superview;
+    
+    self.frontBT = ({
+        NSButton * button = [NSButton checkboxWithTitle:@"置顶" target:self action:@selector(keepAtFrontAction:)];
+        
+        [titleBarView addSubview:button];
+        button;
+    });
+    
+    [self.frontBT mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(0);
+        make.bottom.mas_equalTo(-0);
+        make.right.mas_equalTo(-5);
+    }];
+    
+    // event
+    NSString * front = [self get__keepFront];
+    if (front.integerValue == 0) {
+        self.frontBT.state = NSControlStateValueOn;
+        [self keepAtFront:NO];
+    } else {
+        self.frontBT.state = NSControlStateValueOff;
+        [self keepAtFront:YES];
+    }
 }
 
 
@@ -34,12 +64,35 @@
 }
 
 
-- (void)keepAtFrontAction:(BOOL)front {
+- (void)keepAtFrontAction:(NSButton *)bt {
+    if (bt.state == NSControlStateValueOff) {
+        bt.state = NSControlStateValueOff;
+    } else {
+        bt.state = NSControlStateValueOn;
+    }
+    //NSLog(@"%li", bt.state);
+    
+    [self keepAtFront:bt.state != NSControlStateValueOn];
+}
+
+- (void)keepAtFront:(BOOL)front {
     if (front) {
         [NSApp.windows[0] setLevel:NSNormalWindowLevel];
     }else{
         [NSApp.windows[0] setLevel:NSFloatingWindowLevel];
     }
+    [self save__keepFront:[NSString stringWithFormat:@"%i", front]];
 }
+
+- (void)save__keepFront:(NSString *)__keepFront {
+    [[NSUserDefaults standardUserDefaults] setObject:__keepFront forKey:@"__keepFront"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+- (NSString *)get__keepFront {
+    NSString * info = [[NSUserDefaults standardUserDefaults] objectForKey:@"__keepFront"];
+    return info;
+}
+
 
 @end
